aeb2d4fb3bd6fddb8533eb126d89208dc025789b
annity <azoobkova@gmail.com>
2013-08-04 00:26:45 +0300

    Updated

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buf = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
